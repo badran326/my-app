4f083bb9f397ab47071f3eb0ab3194a41ab59d63
Badr Ahmed <Badr20126162@gmail.com>
2023-06-22 00:56:18 +0300

    pushing my code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className='App-nav'>
+          <ol>
+            <li><a href=''>Home</a></li>
+            <li><a href=''>Gallery</a></li>
+            <li><a href=''>About me</a></li>
+            <li className='badran'>Developer Badran</li>
+            <li className='li-contact'><a href=''>Contact me</a></li>
+            <img src='Badr.jpg'></img>
+          </ol>
+        </nav>
       </header>
+      <body className="App-body">
+      </body>
     </div>
   );
 }
