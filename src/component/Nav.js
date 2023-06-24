bf659a329d6420be93c061821fddd905285f3b66
Badr Ahmed <Badr20126162@gmail.com>
2023-06-24 12:35:30 +0300

    pushing my code

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,16 +1,15 @@
 import '../App.css';
-import { Link } from 'react-router-dom';
 
 function Nav() {
     return (
         <header className="App-header">
             <nav className='App-nav'>
                 <ol>
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/gallery'}>Gallery</Link></li>
-                    <li><Link to={'/about-me'}>About me</Link></li>
+                    <li><a>Home</a></li>
+                    <li><a>Gallery</a></li>
+                    <li><a>About me</a></li>
                     <li className='badran'>Developer Badran</li>
-                    <li className='li-contact'><Link to={'/Contact-me'}>Contact me</Link></li>
+                    <li className='li-contact'><a>Contact me</a></li>
                     <img src='Badr.jpg' alt='Badr'></img>
                 </ol>
             </nav>
--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Gallery from './pages/Gallery';
-import AboutMe from './pages/AboutMe';
-import ContactMe from './pages/ContactMe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/gallery' element={<Gallery/>} />
-      <Route path='/about-me' element={<AboutMe/>}/>
-      <Route path='/contact-me' element={<ContactMe/>} />
-    </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>
 );
 
